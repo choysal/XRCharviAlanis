ad50053ec19c4c6fc5049ffb54c6c51fdebac640
alaniszhao <zhaoalanis@gmail.com>
2025-11-24 16:22:06 -0500

    push to talk works

---
+++ Assets/mic_check.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using Oculus.Voice;   // AppVoiceExperience
+using UnityEngine.InputSystem;
+
+public class PushToTalk : MonoBehaviour
+{
+    public AppVoiceExperience voice;
+    public InputActionProperty buttonAction;
+
+    private bool isHeld = false;
+
+    private void OnEnable()
+    {
+        buttonAction.action.Enable(); 
+        buttonAction.action.started += OnPressed;
+        buttonAction.action.canceled += OnReleased;
+
+        voice.VoiceEvents.OnStoppedListening.AddListener(OnStoppedListening);
+        voice.VoiceEvents.OnFullTranscription.AddListener(OnFull);
+        voice.VoiceEvents.OnPartialTranscription.AddListener(OnPartial);
+    }
+
+    private void OnDisable()
+    {
+        buttonAction.action.started -= OnPressed;
+        buttonAction.action.canceled -= OnReleased;
+
+        voice.VoiceEvents.OnStoppedListening.RemoveListener(OnStoppedListening);
+        voice.VoiceEvents.OnFullTranscription.RemoveListener(OnFull);
+        voice.VoiceEvents.OnPartialTranscription.RemoveListener(OnPartial);
+    }
+
+    private void OnPressed(InputAction.CallbackContext ctx)
+    {
+        isHeld = true;
+        Debug.Log("🎤 Voice ACTIVATED");
+        voice.Activate();
+    }
+
+    private void OnReleased(InputAction.CallbackContext ctx)
+    {
+        isHeld = false;
+        Debug.Log("🛑 Voice DEACTIVATED");
+        voice.Deactivate();
+    }
+
+    private void OnStoppedListening()
+    {
+        // If still holding the button, keep listening
+        if (isHeld)
+        {
+            Debug.Log("🔄 Auto-restarting mic...");
+            voice.Activate();
+        }
+    }
+
+    private void OnPartial(string text)
+    {
+        Debug.Log("🟡 PARTIAL: " + text);
+    }
+
+    private void OnFull(string text)
+    {
+        Debug.Log("🟢 FULL: " + text);
+    }
+}
